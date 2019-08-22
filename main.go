1edd63f86c92c5efda7b7dcc8303fccc401a4c8e
Chengcheng Zhang <943420582@qq.com>
2019-08-22 16:31:27 +0800

    update

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 )
 
 var (
-	localURL = "http://127.0.0.1:9888/"
+	localURL        = "http://127.0.0.1:9888/"
+	listAccountsURL = localURL + "list-accounts"
+	listBalancesURL = localURL + "list-balances"
 )
 
 type Balance struct {
 	AccountID string `json:"account_id"`
 	Amount    uint64 `json:"amount"`
-	Other     interface{}
 }
 
 type Balances struct {
@@ -24,14 +25,11 @@ type Balances struct {
 }
 
 func main() {
-	url := localURL + "list-balances"
-	fmt.Println("URL:>", url)
+	listBalances()
+}
 
-	var jsonStr = []byte(`{
-		"account_alias": "a1"
-		}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	// req.Header.Set("X-Custom-Header", "myvalue")
+func request(URL string, data []byte) []byte {
+	req, err := http.NewRequest("POST", URL, bytes.NewBuffer(data))
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
@@ -41,16 +39,26 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	// fmt.Println("response Status:", resp.Status)
-	// fmt.Println("response Headers:", resp.Header)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
+	return body
+}
 
-	newBalances := new(Balances)
-	if err := json.Unmarshal(body, newBalances); err != nil {
+func listAccounts() {
+
+}
+
+func listBalances() {
+	var data = []byte(`{
+		"account_alias": "a1"
+		}`)
+	body := request(listBalancesURL, data)
+
+	balances := new(Balances)
+	if err := json.Unmarshal(body, balances); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("newBalances:", newBalances)
-	fmt.Println("status: ", newBalances.Status)
-	fmt.Println("amount: ", newBalances.Data[0].Amount)
+	fmt.Println("balances:", balances)
+	fmt.Println("status: ", balances.Status)
+	fmt.Println("amount: ", balances.Data[0].Amount)
 }
